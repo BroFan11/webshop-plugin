eca93f9c3b7480aef51639bc9968d88ed1ded82b
Michał Markiewicz <markiewicz.m@outlook.com>
2017-08-06 09:08:00 +0200

    plugin init

---
+++ src/main/java/com/github/dzikismigol/webshop/WebShop.java
@@ -0,0 +1,17 @@
+package com.github.dzikismigol.webshop;
+
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class WebShop extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
